a0685ce1102bd97b1329b7c0580fd617dabc3952
Flavio Curella <flavio.curella@gmail.com>
2014-03-23 12:27:37 -0500

    added test for cross in operation

--- databuild/tests/test_operator.py
+++ databuild/tests/test_operator.py
@@ -111,6 +111,44 @@ class OperatorTestCase(TestCase):
         sheet_a.append_column("test column")
         book.apply_operation(operation)
         assert sheet_a.get_column('test column') == [3, 5, 7]
+
+    def test_cross_function(self):
+        operation = {
+            "path": "columns.update_column",
+            "description": "",
+            "params": {
+                "sheet": "a",
+                "column": "test column",
+                "facets": [],
+                "expression": {
+                    "language": "lua",
+                    "content": "return cross(row, 'b', 'z', 'id')"
+                }
+            }
+        }
+
+        a_data = [
+            {'id': 1, 'x': 2, 'y': 3},
+            {'id': 2, 'x': 2, 'y': 3.5},
+            {'id': 3, 'x': 1, 'y': 3.5},
+        ]
+        b_data = [
+            {'id': 3, 'z': 3},
+            {'id': 1, 'z': 4},
+            {'id': 2, 'z': 4.5},
+        ]
+
+        book = LocMemBook('project1')
+        sheet_a = book.add_sheet('a', ['id', 'x', 'y'])
+        sheet_b = book.add_sheet('b', ['id', 'z'])
+
+        sheet_a.extend(a_data)
+        sheet_b.extend(b_data)
+
+        sheet_a.append_column("test column")
+        book.apply_operation(operation)
+        assert sheet_a.get_column('test column') == [4, 4.5, 3]
+
     def test_facets(self):
         operation = {
             "path": "columns.update_column",

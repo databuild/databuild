9eaefcdc60aeef9d17d0363fe74cd5c102036a3b
Flavio Curella <flavio.curella@gmail.com>
2014-04-21 17:05:57 -0500

    added missing whitespace

--- databuild/functional.py
+++ databuild/functional.py
@@ -4,6 +4,7 @@ from functools import partial
 def _composed(f, g, *args, **kwargs):
     return f(g(*args, **kwargs))
  
+
 def compose(*a):
     try:
         return partial(_composed, a[0], compose(*a[1:]))

f9c03609257505cdf9d8ebcb0715a67ff0dbca64
Flavio Curella <flavio.curella@gmail.com>
2014-04-27 15:30:15 -0500

    encode on save

--- databuild/operations/core.py
+++ databuild/operations/core.py
@@ -1,3 +1,5 @@
+from databuild.compat import _open
+
 def import_data(workbook, format, filename, sheet, *args, **kwargs):
     workbook.import_data(format, filename, sheet, *args, **kwargs)
 
@@ -5,7 +7,7 @@ def import_data(workbook, format, filename, sheet, *args, **kwargs):
 def export_data(workbook, sheet, format, filename):
     sheet = workbook.sheets[sheet]
     data = sheet.export_data(format)
-    with open(filename, 'wb') as fh:
+    with _open(filename, 'w', encoding='utf-8') as fh:
         fh.write(data)
 
 

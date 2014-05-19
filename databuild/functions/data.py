b92f6d0dbadd3654c9acd6721609d49bbcce35d2
Flavio Curella <flavio.curella@gmail.com>
2014-05-19 13:13:15 -0500

    removed unimplemented function

--- databuild/functions/data.py
+++ databuild/functions/data.py
@@ -15,11 +15,3 @@ def column(environment, book, sheet, sheet_source, column_source, column_key):
     b = book.sheets[sheet]
 
     return environment.copy([a.get(**{column_key: row[column_key]})[column_source] for row in b.all()])
-
-
-def group_by(environment, book):
-    """
-    groups a set of rows by column
-    """
-    raise NotImplementedError()
-

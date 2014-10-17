dd4b55d03faad69b464f28db96b98fd76d512eef
Flavio Curella <flavio.curella@gmail.com>
2014-10-17 12:52:07 -0500

    made command arguments optional

--- databuild/operations/system.py
+++ databuild/operations/system.py
@@ -1,6 +1,7 @@
 import subprocess
 
-
-def call_command(context, command, arguments):
+def call_command(context, command, arguments=None):
+    if arguments is None:
+        arguments = []
     call_args = [command] + arguments
     subprocess.check_call(call_args)
